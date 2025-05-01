a3b519a7a7693bcedaeab63387124c69dd14256f
Joshua Walker <jwalker083@su.edu>
2025-05-01 16:02:52 -0400

    testing: fixes lint error to test preview

--- app.config.ts
+++ app.config.ts
@@ -8,4 +8,4 @@ export default ({ config }: { config: DotenvConfigOutput }) => ({
     EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
     EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
   },
-});;
+});
