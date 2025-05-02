53a4a0096e92383f8f53d0513447bb556012abff
Joshua Walker <jwalker083@su.edu>
2025-05-02 01:30:57 -0400

    testing: adds project id

--- app.config.ts
+++ app.config.ts
@@ -7,5 +7,8 @@ export default ({ config }: { config: DotenvConfigOutput }) => ({
     // These must match your .env to be available at runtime
     EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
     EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    "eas": {
+      "projectId": process.env.PROJECT_ID,
+    },
   },
 });
